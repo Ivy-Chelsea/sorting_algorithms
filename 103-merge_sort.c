1162c0107effe01624ad7109d9302cf8f51d6818
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-10-10 23:48:48 +0300

    fix betty issues

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -15,25 +15,23 @@ void merge_sort_recursive(int *subarr, int *buff, size_t front,
 void merge_split(int *subarr, int *buff, size_t front, size_t mid,
 		 size_t back)
 {
-  size_t a, b, c = 0;
-
-  printf("Merging...\n[left]: ");
-  print_array(subarr + front, mid - front);
-
-  printf("[right]: ");
-  print_array(subarr + mid, back - mid);
-
-  for (a = front, b = mid; a < mid && b < back; c++)
-    buff[c] = (subarr[a] < subarr[b]) ? subarr[a++] : subarr[b++];
-  for(; a < mid; a++)
-    buff[c++] = subarr[a];
-  for(; b < back; b++)
-    buff[c++] = subarr[b];
-  for(a = front, c = 0; a < back; a++)
-    subarr[a] = buff[c++];
-
-  printf("[Done]: ");
-  print_array(subarr + front, back - front);
+	size_t a, b, c = 0;
+	printf("Merging...\n[left]: ");
+	print_array(subarr + front, mid - front);
+	
+	printf("[right]: ");
+	print_array(subarr + mid, back - mid);
+	
+	for (a = front, b = mid; a < mid && b < back; c++)
+		buff[c] = (subarr[a] < subarr[b]) ? subarr[a++] : subarr[b++];
+	for (; a < mid; a++)
+		buff[c++] = subarr[a];
+	for (; b < back; b++)
+		buff[c++] = subarr[b];
+	for (a = front, c = 0; a < back; a++)
+		subarr[a] = buff[c++];
+	printf("[Done]: ");
+	print_array(subarr + front, back - front);
 }
 
 /**
@@ -45,15 +43,15 @@ void merge_split(int *subarr, int *buff, size_t front, size_t mid,
  */
 void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back)
 {
-  size_t mid;
-
-  if (back - front > 1)
-    {
-      mid = front + (back - front) / 2;
-      merge_sort_recursive(subarr, buff, front, mid);
-      merge_sort_recursive(subarr, buff, mid, back);
-      merge_split(subarr, buff, front, mid, back);
-    }
+	size_t mid;
+	
+	if (back - front > 1)
+	{
+		mid = front + (back - front) / 2;
+      		merge_sort_recursive(subarr, buff, front, mid);
+      		merge_sort_recursive(subarr, buff, mid, back);
+      		merge_split(subarr, buff, front, mid, back);
+    	}
 }
 
 /**
@@ -66,15 +64,15 @@ void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back)
  */
 void merge_sort(int *array, size_t size)
 {
-  int *buff;
+	int *buff;
 
-  if(array == NULL || size < 2)
-    return;
+  	if(array == NULL || size < 2)
+    		return;
 
-  buff = malloc(sizeof(int) * size);
-  if (buff == NULL)
-    return;
+  	buff = malloc(sizeof(int) * size);
+  	if (buff == NULL)
+    		return;
 
-  merge_sort_recursive(array, buff, 0, size);
-  free(buff);
+  	merge_sort_recursive(array, buff, 0, size);
+  	free(buff);
 }
