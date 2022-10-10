c56a4aaf5d680ce41c2734c7b24d134499e8f797
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-10-10 22:48:03 +0300

    add blank line

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -27,6 +27,7 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *iter, *insert, *tmp;
+	
 	if (list == NULL || !(*list) || (*list)->next == NULL)
 		return;
 	
