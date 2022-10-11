416da1b57a7b4a11df67f8ef2e7864852f9133d2
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-10-11 21:39:41 +0300

    remove whitespaces

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -16,15 +16,15 @@ void insert_sort_deck_value(deck_node_t **deck);
  */
 int _strcmp(const char *s1, const char *s2)
 {
-  while (*s1 && *s2 && *s1 == *s2)
-    {
-      s1++;
-      s2++;
-    }
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
 
-  if (*s1 != *s2)
-    return (*s1 - *s2);
-  return (0);
+	if (*s1 != *s2)
+		return (*s1 - *s2);
+	return (0);
 }
 
 /**
@@ -35,33 +35,33 @@ int _strcmp(const char *s1, const char *s2)
  */
 char get_value(deck_node_t *card)
 {
-  if(_strcmp(card->card->value, "Ace") == 0)
-    return (0);
-  if(_strcmp(card->card->value, "1") == 0)
-    return (1);
-  if(_strcmp(card->card->value, "2") == 0)
-    return (2);
-  if(_strcmp(card->card->value, "3") == 0)
-    return (3);
-  if(_strcmp(card->card->value, "4") == 0)
-    return (4);
-  if(_strcmp(card->card->value, "5") == 0)
-    return (5);
-  if(_strcmp(card->card->value, "6") == 0)
-    return (6);
-  if(_strcmp(card->card->value, "7") == 0)
-    return (7);
-  if(_strcmp(card->card->value, "8") == 0)
-    return (8);
-  if(_strcmp(card->card->value, "9") == 0)
-    return (9);
-  if(_strcmp(card->card->value, "10") == 0)
-    return (10);
-  if(_strcmp(card->card->value, "Jack") == 0)
-    return (11);
-  if(_strcmp(card->card->value, "Queen") == 0)
-    return (12);
-  return (13);
+	if(_strcmp(card->card->value, "Ace") == 0)
+		return (0);
+	if(_strcmp(card->card->value, "1") == 0)
+		return (1);
+	if(_strcmp(card->card->value, "2") == 0)
+		return (2);
+	if(_strcmp(card->card->value, "3") == 0)
+		return (3);
+	if(_strcmp(card->card->value, "4") == 0)
+		return (4);
+	if(_strcmp(card->card->value, "5") == 0)
+		return (5);
+	if(_strcmp(card->card->value, "6") == 0)
+		return (6);
+	if(_strcmp(card->card->value, "7") == 0)
+		return (7);
+	if(_strcmp(card->card->value, "8") == 0)
+		return (8);
+	if(_strcmp(card->card->value, "9") == 0)
+		return (9);
+	if(_strcmp(card->card->value, "10") == 0)
+		return (10);
+	if(_strcmp(card->card->value, "Jack") == 0)
+		return (11);
+	if(_strcmp(card->card->value, "Queen") == 0)
+		return (12);
+	return (13);
 }
 
 /**
@@ -70,27 +70,27 @@ char get_value(deck_node_t *card)
  */
 void insert_sort_deck_kind(deck_node_t **deck)
 {
-  deck_node_t *iter, *insert, *temp;
+	deck_node_t *iter, *insert, *temp;
 
-  for (iter = (*deck)->next; iter != NULL; iter = temp)
-    {
-      temp = iter->next;
-      insert = iter->prev;
-      while (insert != NULL && insert->card->kind > iter->card->kind)
+	for (iter = (*deck)->next; iter != NULL; iter = temp)
 	{
-	  insert->next = iter->next;
-	  if (iter->next != NULL)
-	    iter->next->prev = insert;
-	  iter->prev = insert->prev;
-	  iter->next = insert;
-	  if (insert->prev != NULL)
-	    insert->prev->next = iter;
-	  else
-	    *deck = iter;
-	  insert->prev = iter;
-	  insert = iter->prev;
+		temp = iter->next;
+		insert = iter->prev;
+		while (insert != NULL && insert->card->kind > iter->card->kind)
+		{
+			insert->next = iter->next;
+			if (iter->next != NULL)
+				iter->next->prev = insert;
+			iter->prev = insert->prev;
+			iter->next = insert;
+			if (insert->prev != NULL)
+				insert->prev->next = iter;
+			else
+				*deck = iter;
+			insert->prev = iter;
+			insert = iter->prev;
+		}
 	}
-    }
 }
 
 /**
@@ -100,29 +100,29 @@ void insert_sort_deck_kind(deck_node_t **deck)
  */
 void insert_sort_deck_value(deck_node_t **deck)
 {
-  deck_node_t *iter, *insert, *temp;
+	deck_node_t *iter, *insert, *temp;
 
-  for (iter = (*deck)->next; iter != NULL; iter = temp)
-    {
-      temp = iter->next;
-      insert = iter->prev;
-      while (insert != NULL &&
-	     insert->card->kind == iter->card->kind &&
-	     get_value(insert) > get_value(iter))
+	for (iter = (*deck)->next; iter != NULL; iter = temp)
 	{
-	  insert->next = iter->next;
-	  if (iter->next != NULL)
-	    iter->next->prev = insert;
-	  iter->prev = insert->prev;
-	  iter->next = insert;
-	  if (insert->prev != NULL)
-	    insert->prev->next = iter;
-	  else
-	    *deck = iter;
-	  insert->prev = iter;
-	  insert = iter->prev;
+		temp = iter->next;
+		insert = iter->prev;
+		while (insert != NULL &&
+		       insert->card->kind == iter->card->kind &&
+		       get_value(insert) > get_value(iter))
+		{
+			insert->next = iter->next;
+			if (iter->next != NULL)
+				iter->next->prev = insert;
+			iter->prev = insert->prev;
+			iter->next = insert;
+			if (insert->prev != NULL)
+				insert->prev->next = iter;
+			else
+				*deck = iter;
+			insert->prev = iter;
+			insert = iter->prev;
+		}
 	}
-    }
 }
 
 /**
@@ -132,9 +132,9 @@ void insert_sort_deck_value(deck_node_t **deck)
  */
 void sort_deck(deck_node_t **deck)
 {
-  if (deck == NULL || *deck == NULL || (*deck)->next == NULL)
-    return;
+	if (deck == NULL || *deck == NULL || (*deck)->next == NULL)
+		return;
 
-  insert_sort_deck_kind(deck);
-  insert_sort_deck_value(deck);
+	insert_sort_deck_kind(deck);
+	insert_sort_deck_value(deck);
 }
