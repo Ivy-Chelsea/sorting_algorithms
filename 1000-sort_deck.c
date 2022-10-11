3d4ca240c087cc8d273a13ba1bc20716a236468a
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-10-11 21:42:42 +0300

    add whitespace where necessary

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -35,31 +35,31 @@ int _strcmp(const char *s1, const char *s2)
  */
 char get_value(deck_node_t *card)
 {
-	if(_strcmp(card->card->value, "Ace") == 0)
+	if (_strcmp(card->card->value, "Ace") == 0)
 		return (0);
-	if(_strcmp(card->card->value, "1") == 0)
+	if (_strcmp(card->card->value, "1") == 0)
 		return (1);
-	if(_strcmp(card->card->value, "2") == 0)
+	if (_strcmp(card->card->value, "2") == 0)
 		return (2);
-	if(_strcmp(card->card->value, "3") == 0)
+	if (_strcmp(card->card->value, "3") == 0)
 		return (3);
-	if(_strcmp(card->card->value, "4") == 0)
+	if (_strcmp(card->card->value, "4") == 0)
 		return (4);
-	if(_strcmp(card->card->value, "5") == 0)
+	if (_strcmp(card->card->value, "5") == 0)
 		return (5);
-	if(_strcmp(card->card->value, "6") == 0)
+	if (_strcmp(card->card->value, "6") == 0)
 		return (6);
-	if(_strcmp(card->card->value, "7") == 0)
+	if (_strcmp(card->card->value, "7") == 0)
 		return (7);
-	if(_strcmp(card->card->value, "8") == 0)
+	if (_strcmp(card->card->value, "8") == 0)
 		return (8);
-	if(_strcmp(card->card->value, "9") == 0)
+	if (_strcmp(card->card->value, "9") == 0)
 		return (9);
-	if(_strcmp(card->card->value, "10") == 0)
+	if (_strcmp(card->card->value, "10") == 0)
 		return (10);
-	if(_strcmp(card->card->value, "Jack") == 0)
+	if (_strcmp(card->card->value, "Jack") == 0)
 		return (11);
-	if(_strcmp(card->card->value, "Queen") == 0)
+	if (_strcmp(card->card->value, "Queen") == 0)
 		return (12);
 	return (13);
 }
