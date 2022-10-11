cc40debf92c0b62fe4ac7ccf332675a119a8ed86
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-10-11 20:57:54 +0300

    sort betty issues

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -16,18 +16,19 @@ void bitonic_seq(int *array, size_t size, size_t start, size_t seq,
 void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
 		   char flow)
 {
-  size_t i, jump = seq / 2;
-  if (seq > 1)
-    {
-      for (i = start; i < start + jump; i++)
+	size_t i, jump = seq / 2;
+
+	if (seq > 1)
 	{
-	  if ((flow == UP && array[i] > array[i + jump]) ||
-	      (flow == DOWN && array[i] < array[i + jump]))
-	    swap_ints(array + i, array + i + jump);
+		for (i = start; i < start + jump; i++)
+		{
+			if ((flow == UP && array[i] > array[i + jump]) ||
+			    (flow == DOWN && array[i] < array[i + jump]))
+				swap_ints(array + i, array + i + jump);
+		}
+		bitonic_merge(array, size, start, jump, flow);
+		bitonic_merge(array, size, start + jump, jump, flow);
 	}
-      bitonic_merge(array, size, start, jump, flow);
-      bitonic_merge(array, size, start + jump, jump, flow);
-    }
 }
 
 /**
@@ -40,21 +41,21 @@ void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
  */
 void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow)
 {
-  size_t cut = seq / 2;
-  char *str = (flow == UP) ? "UP" : "DOWN";
+	size_t cut = seq / 2;
+	char *str = (flow == UP) ? "UP" : "DOWN";
 
-  if (seq > 1)
-    {
-      printf("Merging [%lu/%lu] (%s):\n", seq, size, str);
-      print_array(array + start, seq);
+	if (seq > 1)
+	{
+		printf("Merging [%lu/%lu] (%s):\n", seq, size, str);
+		print_array(array + start, seq);
 
-      bitonic_seq(array, size, start, cut, UP);
-      bitonic_seq(array, size, start + cut, cut, DOWN);
-      bitonic_merge(array, size, start, seq, flow);
+		bitonic_seq(array, size, start, cut, UP);
+		bitonic_seq(array, size, start + cut, cut, DOWN);
+		bitonic_merge(array, size, start, seq, flow);
 
-      printf("Result [%lu/%lu] (%s):\n", seq, size, str);
-      print_array(array + start, seq);
-    }
+		printf("Result [%lu/%lu] (%s):\n", seq, size, str);
+		print_array(array + start, seq);
+	}
 }
 
 /**
@@ -68,8 +69,8 @@ void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow)
  */
 void bitonic_sort(int *array, size_t size)
 {
-  if (array == NULL || size < 2)
-    return;
+	if (array == NULL || size < 2)
+		return;
 
-  bitonic_seq(array, size, 0, size, UP);
+	bitonic_seq(array, size, 0, size, UP);
 }
